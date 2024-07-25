97641488fa2e811bae02d167ef91eec4f5442fc1
Fankrbb <fankrbb@gmail.com>
2024-07-25 15:44:28 +0900

    feat(frontend): preparing for frontend

--- src/main/java/io/github/vkgsim/controller/WebController.java
+++ src/main/java/io/github/vkgsim/controller/WebController.java
@@ -1,39 +1,53 @@
 package io.github.vkgsim.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
-    // Mapping for the home page
-    @GetMapping("/")
-    public String homePage() {
-        return "mainPage";
-    }
-
-    // Mapping for the main page
-    @GetMapping("/mainPage")
-    public String mainPage() {
-        return "mainPage";
-    }
-
-    // Mapping for the mapping page
-    @GetMapping("/mappingPage")
-    public String mappingPage() {
-        return "mappingPage";
-    }
-
-    // Mapping for the query page
-    @GetMapping("/queryPage")
-    public String queryPage() {
-        return "queryPage";
-    }
-
-    // Mapping for the error page
-    @GetMapping("/error")
-    public String errorPage() {
-        return "errorPage";
+    // Handle all routes except those containing a dot (i.e., exclude static resources)
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
     }
 }
 
+//package io.github.vkgsim.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class WebController {
+//
+//    // Mapping for the home page
+//    @GetMapping("/")
+//    public String homePage() {
+//        return "mainPage";
+//    }
+//
+//    // Mapping for the main page
+//    @GetMapping("/mainPage")
+//    public String mainPage() {
+//        return "mainPage";
+//    }
+//
+//    // Mapping for the mapping page
+//    @GetMapping("/mappingPage")
+//    public String mappingPage() {
+//        return "mappingPage";
+//    }
+//
+//    // Mapping for the query page
+//    @GetMapping("/queryPage")
+//    public String queryPage() {
+//        return "queryPage";
+//    }
+//
+//    // Mapping for the error page
+//    @GetMapping("/error")
+//    public String errorPage() {
+//        return "errorPage";
+//    }
+//}
